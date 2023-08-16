dd38db2bf2aa6001444e715dce077bb3771f8fdb
Sergei Veresaev <138131897+veresaev@users.noreply.github.com>
2023-08-17 00:05:32 +0300

    Added light viewers, start button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 30
+        // opacity
+        redLight.layer.opacity = 0.3
+        yellowLight.layer.opacity = 0.3
+        greenLight.layer.opacity = 0.3
+        // cornerRadius
+        redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
     }
 
-
+    @IBAction func startButtonAction() {
+        if redLight.layer.opacity == 0.3 &&
+            yellowLight.layer.opacity == 0.3 &&
+            greenLight.layer.opacity == 0.3 {
+            startButton.setTitle("NEXT", for: .normal)
+            
+            redLight.layer.opacity = 1
+        } else if redLight.layer.opacity == 1 &&
+                    yellowLight.layer.opacity == 0.3 &&
+                    greenLight.layer.opacity == 0.3 {
+            redLight.layer.opacity = 0.3
+            yellowLight.layer.opacity = 1
+        } else if redLight.layer.opacity == 0.3 &&
+                    yellowLight.layer.opacity == 1 &&
+                    greenLight.layer.opacity == 0.3 {
+            yellowLight.layer.opacity = 0.3
+            greenLight.layer.opacity = 1
+        } else if redLight.layer.opacity == 0.3 &&
+                    yellowLight.layer.opacity == 0.3 &&
+                    greenLight.layer.opacity == 1 {
+            greenLight.layer.opacity = 0.3
+            redLight.layer.opacity = 1
+        }
+    }
+    
 }
 
